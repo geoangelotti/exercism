5f3f5dfaabf1d9437f0ef2a1e997e853070d2415
Geo Angelopoulos <geo.angelotti@gmail.com>
2023-06-05 17:52:26 +0200

    feat: rust next exercises
    * leap
    * grains
    * reverse-string

---
+++ rust-exercises/grains/src/lib.rs
@@ -0,0 +1,78 @@
+pub fn square(s: u32) -> u64 {
+    if (1..65).contains(&s) {
+        return 2u64.pow(s - 1);
+    }
+    panic!("Square must be between 1 and 64")
+}
+
+pub fn total() -> u64 {
+    u64::MAX
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn process_square_case(input: u32, expected: u64) {
+        assert_eq!(square(input), expected);
+    }
+
+    #[test]
+    /// 1
+    fn test_1() {
+        process_square_case(1, 1);
+    }
+
+    #[test]
+    /// 2
+    fn test_2() {
+        process_square_case(2, 2);
+    }
+
+    #[test]
+    /// 3
+    fn test_3() {
+        process_square_case(3, 4);
+    }
+
+    #[test]
+    /// 4
+    fn test_4() {
+        process_square_case(4, 8);
+    }
+    //NEW
+
+    #[test]
+    /// 16
+    fn test_16() {
+        process_square_case(16, 32_768);
+    }
+
+    #[test]
+    /// 32
+    fn test_32() {
+        process_square_case(32, 2_147_483_648);
+    }
+
+    #[test]
+    /// 64
+    fn test_64() {
+        process_square_case(64, 9_223_372_036_854_775_808);
+    }
+
+    #[test]
+    #[should_panic(expected = "Square must be between 1 and 64")]
+    fn test_square_0_raises_an_exception() {
+        square(0);
+    }
+
+    #[test]
+    #[should_panic(expected = "Square must be between 1 and 64")]
+    fn test_square_greater_than_64_raises_an_exception() {
+        square(65);
+    }
+
+    #[test]
+    fn test_returns_the_total_number_of_grains_on_the_board() {
+        assert_eq!(total(), 18_446_744_073_709_551_615);
+    }
+}
---
+++ rust-exercises/leap/src/lib.rs
@@ -0,0 +1,108 @@
+pub fn is_leap_year(year: u64) -> bool {
+    if remainder_of(year, 400) {
+        return true;
+    }
+    if remainder_of(year, 100) {
+        return false;
+    }
+    if remainder_of(year, 4) {
+        return true;
+    }
+    return false;
+}
+
+fn remainder_of(year: u64, n: u64) -> bool {
+    year % n == 0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn process_leapyear_case(year: u64, expected: bool) {
+        assert_eq!(is_leap_year(year), expected);
+    }
+
+    #[test]
+    fn test_year_not_divisible_by_4_common_year() {
+        process_leapyear_case(2015, false);
+    }
+
+    #[test]
+    fn test_year_divisible_by_2_not_divisible_by_4_in_common_year() {
+        process_leapyear_case(1970, false);
+    }
+
+    #[test]
+    fn test_year_divisible_by_4_not_divisible_by_100_leap_year() {
+        process_leapyear_case(1996, true);
+    }
+
+    #[test]
+    fn test_year_divisible_by_4_and_5_is_still_a_leap_year() {
+        process_leapyear_case(1960, true);
+    }
+
+    #[test]
+    fn test_year_divisible_by_100_not_divisible_by_400_common_year() {
+        process_leapyear_case(2100, false);
+    }
+
+    #[test]
+    fn test_year_divisible_by_100_but_not_by_3_is_still_not_a_leap_year() {
+        process_leapyear_case(1900, false);
+    }
+
+    #[test]
+    fn test_year_divisible_by_400_leap_year() {
+        process_leapyear_case(2000, true);
+    }
+
+    #[test]
+    fn test_year_divisible_by_400_but_not_by_125_is_still_a_leap_year() {
+        process_leapyear_case(2400, true);
+    }
+
+    #[test]
+    fn test_year_divisible_by_200_not_divisible_by_400_common_year() {
+        process_leapyear_case(1800, false);
+    }
+
+    #[test]
+    fn test_any_old_year() {
+        process_leapyear_case(1997, false);
+    }
+
+    #[test]
+    fn test_early_years() {
+        process_leapyear_case(1, false);
+        process_leapyear_case(4, true);
+        process_leapyear_case(100, false);
+        process_leapyear_case(400, true);
+        process_leapyear_case(900, false);
+    }
+
+    #[test]
+    fn test_century() {
+        process_leapyear_case(1700, false);
+        process_leapyear_case(1800, false);
+        process_leapyear_case(1900, false);
+    }
+
+    #[test]
+    fn test_exceptional_centuries() {
+        process_leapyear_case(1600, true);
+        process_leapyear_case(2000, true);
+        process_leapyear_case(2400, true);
+    }
+
+    #[test]
+    fn test_years_1600_to_1699() {
+        let incorrect_years = (1600..1700)
+            .filter(|&year| is_leap_year(year) != (year % 4 == 0))
+            .collect::<Vec<_>>();
+        if !incorrect_years.is_empty() {
+            panic!("incorrect result for years: {incorrect_years:?}");
+        }
+    }
+}
